afee0a63b71b9e7809373a65f373dc26e4251d52
Astom Ahashie <astomeli@outlook.com>
2022-06-21 20:48:21 +0000

    add transation model

---
+++ db/migrate/20220621203957_create_transactions.rb
@@ -0,0 +1,15 @@
+class CreateTransactions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :transactions do |t|
+      t.string :transaction_id, null: false
+      t.string :customer_id, null: false
+      t.float :input_amount, null: false
+      t.string :input_currency, null: false
+      t.float :output_amount, null: false
+      t.string :output_currency, null: false
+
+      t.timestamps
+    end
+    add_index :transactions, :transaction_id, unique: true
+  end
+end
