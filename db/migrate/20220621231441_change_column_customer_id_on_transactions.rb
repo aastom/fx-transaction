31eac07486d35f2cb8eafbc22ffbe18c0d472a4c
Astom Ahashie <astomeli@outlook.com>
2022-06-21 23:23:40 +0000

    change type from uuid to string on customer_id

---
+++ db/migrate/20220621231441_change_column_customer_id_on_transactions.rb
@@ -0,0 +1,5 @@
+class ChangeColumnCustomerIdOnTransactions < ActiveRecord::Migration[7.0]
+  def change
+    change_column :transactions, :customer_id, :string, null: false, using: 'customer_id::varchar'
+  end
+end
