ba501a1dced77fcc706d96a6b18c439a278f10d5
Astom Ahashie <astomeli@outlook.com>
2022-06-22 08:18:25 +0000

    add validations for transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
+  validates :customer_id, :input_amount, :input_currency, :output_currency, presence: true
+  validates :input_amount, numericality: true
+
   def self.calculate_output_amount(input_amount)
     input_amount * 2.3
   end
